c0ecd5326f72aaffcf8fa56d45ec9c9ce4cceb08
Svitlana Dyshkant <110805508+SvitlanaDy@users.noreply.github.com>
2023-04-30 16:03:25 +0300

    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,106 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import Modal from 'components/modal/Modal';
+import SearchBar from 'components/searchbar/SearchBar';
+import Loader from 'components/loader/Loader';
+import Button from 'components/button/Button';
+import ImageGallery from 'components/imageGallery/ImageGallery';
+import { Component } from 'react';
+import { getImages } from 'fetch/fetch';
+import css from '../components/App.module.css';
+// import { nanoid } from 'nanoid'
+
+class App extends Component{
+  state = {
+    searchQuery: '',
+    images: [],
+    page: 1,
+    status: 'idle',
+    selectedImage: null,
+    isShowModal: false,
+    isShowLoadmore: false,
+  };
+
+    componentDidUpdate( prevProps, prevState ) {
+   
+      if (
+        prevState.searchQuery !== this.state.searchQuery ||
+        prevState.page !== this.state.page
+      ) {
+        this.fetchImages();
+      }
+    }
+
+    fetchImages = () => {
+    const {page, searchQuery} = this.state;
+    getImages (page, searchQuery).then(images => {
+      this.setState(prevState => ({images : [...prevState.images, ...images.hits],
+        isShowLoadmore:this.state.page < Math.ceil(images.totalHits/12),
+        status:'resolved'
+      }))
+      console.log(images)
+    })  
+    .catch(error => this.setState({error, status:'rejected'}))
+   
+  }
+ 
+  // showModal = () => {
+  //   this.setState({isShowModal: true})
+  // }
+
+  // hideModal = () => {
+  //   this.setState({isShowModal: false})
+  // }
+
+  createSearchQuery = searchQuery => {
+    this.setState({
+      searchQuery,
+      images: [],
+      page: 1,
+      status: 'pending',
+    });
+  };
+
+toggleModal = () => {
+this.setState(({ isShowModal }) => ({
+  isShowModal: !isShowModal,
+}));
+};
+
+
+onSelectImage = largeImageURL => {
+  this.setState({ selectedImage: largeImageURL, isShowModal: true });
+};
+
+handleLoadmore = () => {
+  this.setState(
+    prevState => ({
+      page: prevState.page + 1,
+    })
   );
 };
+
+
+render() {
+
+
+const { isShowModal, status, images, selectedImage } = this.state;
+return(
+  <div>
+     <SearchBar onSubmit = {this.createSearchQuery}/>
+      { status === 'pending' && <Loader/>}
+      {/* { status === 'rejected' && </>} */}
+      { status === 'resolved' && (
+        <div>{images.length > 0 ? (<>
+        <ImageGallery images = {images} onSelect = {this.onSelectImage}  />
+        {this.state.isShowLoadmore && (<Button onClick = {this.handleLoadmore} >Load More</Button>)}
+      
+        </>):(<h1 className={css.appHeader}> Nothing is here, pls enter smth else</h1>)}
+        </div>
+        )}
+     {isShowModal && (<Modal onClose={this.toggleModal} selectedImage={selectedImage}/>)}
+  </div>
+);
+}
+}
+
+
+export default App;
---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,60 @@
+import css from 'components/modal/Modal.module.css'
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Modal extends Component {
+    static propTypes = {
+        onClose: PropTypes.func.isRequired,
+        selectedImage: PropTypes.string.isRequired,
+      };
+
+
+    componentDidMount(){
+    console.log('Modal componentDidMount');
+    window.addEventListener('keydown' , e => {
+        console.log(e.code);
+        if(e.code === 'Escape'){
+            console.log('Push ESC, close the Modal window');
+        }
+    });
+
+    }
+
+
+componentWillUnmount(){
+    console.log('Modal componentWilUnmount');
+    window.removeEventListener('keydown' , e => {
+        console.log(e.code);
+        if(e.code === 'Escape'){
+            console.log('Push ESC, close the Modal window');
+        }
+    });
+}
+handleBackdropClick = event => {
+    if (event.currentTarget === event.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return (
+      <div className={css.backdrop} onClick={this.handleBackdropClick}>
+        <div className={css.modal}>
+          <img src={this.props.selectedImage} alt="" />
+        </div>
+      </div>
+    );
+  }
+}
+
+// render(){
+//     return(
+//         <div className='Modal__backdrop'>
+//             <div className='Modal__content'>(this.props.children)
+//             <img src={this.props.selectedImage} alt="" />
+//             </div>
+//         </div>)
+    
+// }
+// }
+
